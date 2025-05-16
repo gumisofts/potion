9e19c3cb97a43c1f3ebf8e0d0a76aba07f1fe2bc
Murad <nuradhussen082@gmail.com>
2025-05-16 22:32:38 +0300

    cron job testing with zappa

--- tasks_handler/tasks.py
+++ tasks_handler/tasks.py
@@ -40,3 +40,43 @@ def handle_one_subscription(user_subscription):
             days=user_subscription.subscription.frequency
         )
         user_subscription.save()
+
+
+def subscriptions_payment():
+    subscriptions = UserSubscription.objects.filter(
+        is_active=True, next_billing_date=timezone.now()
+    )
+
+    for subscription in subscriptions:
+        one_subscription_payment(subscription)
+
+
+def one_subscription_payment(user_subscription):
+
+    with transaction.atomic():
+
+        user = user_subscription.user
+        from_wallet = user.wallet
+        to_be_deducted_amount = user_subscription.subscription.fixed_price
+        to_wallet = user_subscription.subscription.bussines.wallet
+
+        tr = Transaction.objects.create(
+            from_wallet=from_wallet,
+            to_wallet=to_wallet,
+            amount=to_be_deducted_amount,
+            remarks=f"Payment for subscription on {user_subscription.subscription.name}",
+            status="pending",
+        )
+
+        user_subscription.next_billing_date = timezone.now() + timedelta(
+            days=user_subscription.subscription.frequency
+        )
+        user_subscription.save()
+
+
+def test_crons():
+    print("test crons")
+    # subscriptions_payment()
+    # dispatch_subscription_payment()
+    # handle_one_subscription()
+    # one_subscription_payment()
