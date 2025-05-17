60cd2ce4517ffae112ef9b3a02e10c9006256922
Murad <nuradhussen082@gmail.com>
2025-05-17 12:32:08 +0300

    formatted

--- subscriptions/signals.py
+++ subscriptions/signals.py
@@ -4,8 +4,9 @@ from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Subscription, UserSubscription
 from wallets.models import Transaction
+
+from .models import Subscription, UserSubscription
 
 logger = logging.getLogger(__name__)
 
